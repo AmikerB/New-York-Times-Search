cb9d6e3cb21027c23b238450f56ef79ff74ffd52
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-02-02 17:43:39 +0000

    Updated id tags. ClearBtn on click function.

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,30 +1,41 @@
+$("#searchBtn").on("click", function (event) {
+  event.preventDefault();
 
+  const queryURL =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV";
 
-$('#searchButton').on('click', function (event) {
+  // retrieve search input text
+  const searchInput = $("#searchInput").val();
+  console.log(searchInput);
+  // retrieve number of records
+  const numberInput = $("#numberInput").val();
+  console.log(numberInput);
+  // retrieve search start year
+  const startYearInput = $("#startYearInput").val();
+  console.log(startYearInput);
+  // retrieve search end year
+  const endYearInput = $("#endYearInput").val();
+  console.log(endYearInput);
 
-    event.preventDefault();
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
 
-    const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV';
+    // display title of article to page
 
-    // link to search input text
-    const searchInput = $('#search-term').val();
-    // value of users input year
-    const searchYear = $('#search-year').val();
+    // display article year
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
+    // display link to article
+  });
+});
 
+$("#clearBtn").on("click", function (event) {
+  event.preventDefault();
 
-
-        // display title of article to page 
-
-        // display article year 
-
-        // display link to article 
-
-    });
-
+  $("#searchInput").val("");
+  $("#numberInput").val("");
+  $("#startYearInput").val("");
+  $("#endYearInput").val("");
 });
