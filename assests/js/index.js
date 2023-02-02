a4d08c5cb707a9db4e91ebba47cd54795c4974c0
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-02-02 16:40:42 +0000

    event listener added

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,24 +1,15 @@
 
 
-function searchForArticle() {
-
-    const urlLink = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV';
-
-    // value of user input 
-
-    // value of users input year
-
-    //
-
-}
-
-
-
 $('#searchButton').on('click', function (event) {
 
     event.preventDefault();
 
-    const queryURL = searchForArticle();
+    const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV';
+
+    // link to search input text
+    const searchInput = $('#search-term').val();
+    // value of users input year
+    const searchYear = $('#search-year').val();
 
     $.ajax({
         url: queryURL,
@@ -26,13 +17,14 @@ $('#searchButton').on('click', function (event) {
     }).then(function (response) {
         console.log(response);
 
-        // link to search input text
-        searchInput = $('#search-term').val();
-
-        // title 
 
 
-        // 
+        // display title of article to page 
+
+        // display article year 
+
+        // display link to article 
+
     });
 
 });
