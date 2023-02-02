3162fbee318476615765e5d9fab541d45b5c9801
AmikerB <118816092+AmikerB@users.noreply.github.com>
2023-02-02 16:30:32 +0000

    js layout

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,8 +1,38 @@
-const queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV';
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
+
+function searchForArticle() {
+
+    const urlLink = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=6BqgyH6qCUGIQC1cCyCWzPvF1rs4OqNV';
+
+    // value of user input 
+
+    // value of users input year
+
+    //
+
+}
+
+
+
+$('#searchButton').on('click', function (event) {
+
+    event.preventDefault();
+
+    const queryURL = searchForArticle();
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+
+        // link to search input text
+        searchInput = $('#search-term').val();
+
+        // title 
+
+
+        // 
+    });
 
 });
